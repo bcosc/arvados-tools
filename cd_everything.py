554f5f0413bfdb7772eb18b9b0b93318e2b66be7
bcosc <bcosc@bu.edu>
2017-03-10 20:03:45 +0000

    add list pipeline instances and add more info to each list

--- cd_everything.py
+++ cd_everything.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 
 import arvados
+import re
 
 def list_subprojects(owner_uuid):
   # List subprojects with owner_uuid=owner_uuid
   list = []
   call = arvados.api().groups().list(filters=[["owner_uuid","=",owner_uuid]]).execute()
   for i in xrange(0,call['items_available']):
-    list.append(call['items'][i]['name'])
+    list.append("%s, %s" % (call['items'][i]['name'], call['items'][i]['uuid']))
+  return list
+
+def list_data_collections(owner_uuid):
+  # List collections with owner_uuid=owner_uuid
+  list = []
+  call = arvados.api().collections().list(filters=[["owner_uuid","=",owner_uuid]]).execute()
+  for i in xrange(0,call['items_available']):
+    list.append("%s, %s, %s" %( call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']))
+  return list
+
+def list_pipeline_instances(owner_uuid):
+  # List pipeline instances with owner_uuid=owner_uuid
+  list = []
+  call = arvados.api().pipeline_instances().list(filters=[["owner_uuid","=",owner_uuid]]).execute()
+  for i in xrange(0,call['items_available']):
+    list.append("%s, %s" %( call['items'][i]['name'], call['items'][i]['uuid']))
   return list
 
 def list_project_uuid_with_name(project_name):
@@ -23,7 +40,16 @@ def run_tests():
 
 def main():
   parent_project = raw_input('Whats the name of the parent project? ')
-  print list_subprojects(list_project_uuid_with_name(parent_project))
+  tab = raw_input('What tab do you want to see? ')
+  if re.match('subprojects', tab):
+    for item in list_subprojects(list_project_uuid_with_name(parent_project)):
+      print item
+  if re.match('data collections', tab):
+    for item in list_data_collections(list_project_uuid_with_name(parent_project)):
+      print item
+  if re.match('pipeline instances', tab):
+    for item in list_data_collections(list_project_uuid_with_name(parent_project)):
+      print item
 
 if __name__ == '__main__':
   main()
