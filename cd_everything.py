69aabf90349e2f1c1ba57f82fb1e1fc495ff70d3
bcosc <bcosc@bu.edu>
2017-03-15 18:40:06 +0000

    fixed new line in cd_everything.py

--- cd_everything.py
+++ cd_everything.py
@@ -7,8 +7,7 @@ import sys
 def list_subprojects(owner_uuid):
   # List subprojects with owner_uuid=owner_uuid
   list = []
-  call = arvados.api().groups().list(filters=[["owner_uuid","=",owner_uuid]], limit=call['items_avai
-lable'])).execute()
+  call = arvados.api().groups().list(filters=[["owner_uuid","=",owner_uuid]], limit=call['items_available'])).execute()
   for i in xrange(0,call['items_available']):
     list.append("%s %s" % (call['items'][i]['name'], call['items'][i]['uuid']))
   return list
@@ -23,8 +22,7 @@ def list_all_projects():
 def list_data_collections(owner_uuid):
   # List collections with owner_uuid=owner_uuid
   list = []
-  call = arvados.api().collections().list(filters=[["owner_uuid","=",owner_uuid]], limit=call['items_avai
-lable'])).execute()
+  call = arvados.api().collections().list(filters=[["owner_uuid","=",owner_uuid]], limit=call['items_available'])).execute()
   for i in xrange(0,call['items_available']):
     list.append("%s %s %s" %( call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']))
   return list
