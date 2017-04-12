c0ee0b316aadccba3c974630d88de227a9c4c835
bcosc <bcosc@bu.edu>
2017-04-12 18:32:52 +0000

    added default limit

--- search_for_files.py
+++ search_for_files.py
@@ -12,16 +12,19 @@ def main():
 
   parser = argparse.ArgumentParser()
   parser.add_argument(
-        '-f', '--file', dest='file', required=False, help="File name or beginning of file name")
+        '-f', '--file', dest='file', required=True, help="File name or beginning of file name")
   parser.add_argument(
         '-l', '--limit', dest='limit', required=False, help="Limit of api calls you want")
   options = parser.parse_args()
 
   file = options.file
-  limit = options.limit
+  if not options.limit:
+    limit = '10'
+  else:
+    limit = options.limit
 
   call = arvados.api().collections().list(filters=[["any","@@",file+"%:*"]], limit=limit).execute()
-  print call['items_available']
+  print "There are %s available files on the cluster, showing you the first %s." % (call['items_available'], limit)
   for i in xrange(0,int(limit)):#call['items_available']):
     print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']
 
