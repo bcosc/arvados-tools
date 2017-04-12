c6a659927d98e051ca8ef8dcaee1e452be997443
bcosc <bcosc@bu.edu>
2017-04-12 15:58:26 +0000

    added limit for search_for_files.py

--- search_for_files.py
+++ search_for_files.py
@@ -3,10 +3,15 @@
 import arvados
 import sys
 
-file = sys.argv[1]
+if len(sys.argv) <= 1:
+  print "Usage: python search_for_files.py beginning_of_file_name"
+  sys.exit(0)
 
-call = arvados.api().collections().list(filters=[["any","@@",file+"%:*"]], limit=2).execute()
+file = sys.argv[1]
+limit = sys.argv[2]
+
+call = arvados.api().collections().list(filters=[["any","@@",file+"%:*"]], limit=limit).execute()
 print call['items_available']
-for i in xrange(0,2):#call['items_available']):
+for i in xrange(0,int(limit)):#call['items_available']):
   #print call['items'][i], 
   print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']
