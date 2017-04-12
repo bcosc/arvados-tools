c04f1e2f79127a1a1495a75cf454d566ebc85431
bcosc <bcosc@bu.edu>
2017-04-12 16:04:27 +0000

    added argument parser

--- search_for_files.py
+++ search_for_files.py
@@ -2,16 +2,29 @@
 
 import arvados
 import sys
+import argparse
 
-if len(sys.argv) <= 1:
-  print "Usage: python search_for_files.py beginning_of_file_name"
-  sys.exit(0)
+def main():
 
-file = sys.argv[1]
-limit = sys.argv[2]
+  if len(sys.argv) <= 1:
+    print "Usage: python search_for_files.py -f beginning_of_file_name -l number"
+    sys.exit(0)
 
-call = arvados.api().collections().list(filters=[["any","@@",file+"%:*"]], limit=limit).execute()
-print call['items_available']
-for i in xrange(0,int(limit)):#call['items_available']):
-  #print call['items'][i], 
-  print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+        '-f', '--file', dest='file', required=False, help="File name or beginning of file name")
+  parser.add_argument(
+        '-l', '--limit', dest='limit', required=False, help="Limit of api calls you want")
+  options = parser.parse_args()
+
+  file = options.file
+  limit = options.limit
+
+  call = arvados.api().collections().list(filters=[["any","@@",file+"%:*"]], limit=limit).execute()
+  print call['items_available']
+  for i in xrange(0,int(limit)):#call['items_available']):
+    print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']
+
+if __name__ == '__main__':
+    main()
+
