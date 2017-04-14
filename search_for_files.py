a26e166733d574b8f0a14b007698c75f0eb1c201
bcosc <bcosc@bu.edu>
2017-04-14 20:27:34 +0000

    added modified at for search_for_files.py

--- search_for_files.py
+++ search_for_files.py
@@ -7,7 +7,7 @@ import argparse
 def main():
 
   if len(sys.argv) <= 1:
-    print "Usage: python search_for_files.py -f beginning_of_file_name -l number"
+    print "Usage: python search_for_files.py -f beginning_of_file_name -l limit"
     sys.exit(0)
 
   parser = argparse.ArgumentParser()
@@ -34,6 +34,7 @@ def main():
     collection_filter = ["name","like", "%"+collection+"%"]
     filters.append(list(collection_filter))
 
+  print filters
   call = arvados.api().collections().list(filters=filters, limit=limit).execute()
 
   if int(call['items_available']) < int(limit):
@@ -42,7 +43,7 @@ def main():
   print "There are %s available files on the cluster, showing you the first %s." % (call['items_available'], limit)
 
   for i in xrange(0,int(limit)):
-    print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash']
+    print call['items'][i]['name'], call['items'][i]['uuid'], call['items'][i]['portable_data_hash'], call['items'][i]['modified_at']
 
 if __name__ == '__main__':
     main()
