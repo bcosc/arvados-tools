3f28139079e8504145d6f6fe34465fed16463e18
bcosc <bcosc@curoverse.com>
2017-04-28 16:01:06 -0400

    removed collection checking in subscribe_pipelines.py and added notifications

--- subscribe_pipelines.py
+++ subscribe_pipelines.py
@@ -2,11 +2,14 @@
 
 import arvados
 import arvados.events
+import subprocess
 
 # 'ev' is a dict containing the log table record describing the change.
 def on_message(ev):
-    if ev.get("event_type") == "update" and ev.get("object_kind") == "arvados#pipelineInstance" and ev["properties"]["new_attributes"]["state"] == "Complete":
-        print "Pipeline %s, %s is Complete" % (ev["object_uuid"], ev["properties"]["new_attributes"]["name"])
+  if ev.get("event_type") == "update" and ev.get("object_kind") == "arvados#pipelineInstance" and ev["properties"]["new_attributes"]["state"] == "Complete":
+    msg = "Pipeline %s, %s is Complete" % (ev["object_uuid"], ev["properties"]["new_attributes"]["name"])
+    print msg
+    subprocess.check_call(['notify-send', 'Pipeline Complete', msg, '--icon=dialog-information'])
 
 api = arvados.api("v1")
 ws = arvados.events.subscribe(api, [], on_message)
