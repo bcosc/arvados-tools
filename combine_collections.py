be824408c1ec79560d9d2aaf73d1a90959d523aa
bcosc <bcosc@bu.edu>
2017-07-19 15:21:16 +0000

    added combine_collections.py

---
+++ combine_collections.py
@@ -0,0 +1,19 @@
+import arvados
+import arvados.collection
+import sys
+
+api = arvados.api('v1')
+project_uuid = sys.argv[1]
+
+collection_uuids = []
+for arg in sys.argv[2:len(sys.argv)]:
+  print arg
+  collection_uuids.append(arg)
+
+combined_manifest = ""
+for u in collection_uuids:
+    c = api.collections().get(uuid=u).execute()
+    combined_manifest += c["manifest_text"]
+
+newcol = arvados.collection.Collection(combined_manifest)
+newcol.save_new(name="foobar", owner_uuid=project_uuid)
