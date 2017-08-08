a1c305cc6cb90e14c487475131ca3a5e05540b3a
bcosc <bcosc@curoverse.com>
2017-08-08 13:25:32 -0400

    added try to subscribe_pipelines because of 200 status message

--- subscribe_pipelines.py
+++ subscribe_pipelines.py
@@ -9,11 +9,14 @@ def on_message(ev):
   object_kind_options = ['arvados#pipelineInstance', 'arvados#job']
   properties_options = ['Complete', 'Failed']
   event_type_options = ['update', 'create']
-  if ev.get("event_type") in event_type_options and ev.get("object_kind") in object_kind_options and ev["properties"]["new_attributes"]["state"] in properties_options:
-    print "%s %s %s" % (ev.get("object_kind"), ev["object_uuid"], ev["properties"]["new_attributes"]["state"])
-  if ev["properties"]["new_attributes"]["state"] == "Failed"
-    header = "%s %s %s" % (ev.get("object_kind"), ev["object_uuid"], ev["properties"]["new_attributes"]["state"])
-    subprocess.check_call(['email-me.py', '-d', header])
+  try:
+    if ev.get("event_type") in event_type_options and ev.get("object_kind") in object_kind_options and ev["properties"]["new_attributes"]["state"] in properties_options:
+      print "%s %s %s" % (ev.get("object_kind"), ev["object_uuid"], ev["properties"]["new_attributes"]["state"])
+    if ev["properties"]["new_attributes"]["state"] == "Failed":
+      header = "%s %s %s" % (ev.get("object_kind"), ev["object_uuid"], ev["properties"]["new_attributes"]["state"])
+      subprocess.check_call(['email-me.py', '-d', header])
+  except:
+    pass
 
 api = arvados.api("v1")
 ws = arvados.events.subscribe(api, [], on_message)
