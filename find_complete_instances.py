d83054379bac5010b22274b3b585831894c142b6
bcosc <bcosc@bu.edu>
2017-08-18 14:37:04 +0000

    find_complete_instances.py works for both containers and instances

--- find_complete_instances.py
+++ find_complete_instances.py
@@ -7,13 +7,38 @@ Finds complete instances with a user input name.
 import arvados
 import sys
 
-name = sys.argv[1]
-filter = "%" + name + "%"
+def pipeline_instance(name):
+  filter = "%" + name + "%"
+  completes = ""
+  call = arvados.api().pipeline_instances().list(filters=[["name","like",filter]]).execute()
+  total = call['items_available']
+  if total != 0:
+    print "Pipeline instance uuid, Pipeline instance name, Pipeline instance finish time"
+  for num in xrange(0,total):
+    if call['items'][num]['state'] == 'Complete': # api returns in order of time by default so this should get the latest complete instance.
+      completes = call['items'][num]['uuid']
+      print "%s, %s, %s" % (completes, call['items'][num]['name'], call['items'][num]['modified_at'])
+  print
 
-completes = ""
-call = arvados.api().pipeline_instances().list(filters=[["name","like",filter]]).execute()
-total = call['items_available']
-for num in xrange(0,total):
-  if call['items'][num]['state'] == 'Complete': # api returns in order of time by default so this should get the latest complete instance.
-    completes = call['items'][num]['uuid']
-    print "%s %s" % (completes, call['items'][num]['name'])
+def container_request(name):
+  filter = "%" + name + "%"
+  completes = ""
+  cr = arvados.api().container_requests().list(filters=[["name","like",filter]]).execute()
+  total = cr['items_available']
+  if total != 0:
+    print "Container request uuid, Container request name, Container request finish time"
+  for num in xrange(0,total):
+    c = cr['items'][num]['container_uuid']
+    c_resp = arvados.api().containers().list(filters=[["uuid","=",c]]).execute()
+    if c_resp['items'][0]['state'] == 'Complete': # api returns in order of time by default so this should get the latest complete instance.
+      completes = cr['items'][num]['uuid']
+      print "%s, %s, %s" % (completes, cr['items'][num]['name'], cr['items'][num]['modified_at'])
+
+def main():
+  name = sys.argv[1]
+  pipeline_instance(name)
+  container_request(name)
+
+if __name__ == '__main__':
+    main()
+
