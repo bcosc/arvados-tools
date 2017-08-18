a58a5eb41418a032f2f2a6b51a5dac64f36d587c
bcosc <bcosc@bu.edu>
2017-08-18 14:17:58 +0000

    ls_output_collections.py works for containers and pipeline instances, and removes need for keep-mount

--- ls_output_collections.py
+++ ls_output_collections.py
@@ -5,6 +5,8 @@ import re
 import subprocess
 import sys
 import os
+from arvados.collection import Collection
+import argparse
 
 """
 This script allows users to take a CWL pipeline instance and find outputs 
@@ -13,29 +15,59 @@ for multiple patterns of jobs and print an ls -lah of them.
 """
 # TODO recursively ls collections
 
-if len(sys.argv) <= 2:
-  print "Usage: python ls_output_collections.py pi_uuid keep_mount job_pattern1 job_pattern2 ..."
-  sys.exit(0)
+def container_request(uuid, patterns):
+  cr_uuid = uuid
+  cr = arvados.api().container_requests().list(filters=[["uuid", "=", cr_uuid]]).execute()
+  c_uuid = cr.items()[1][1][0]['container_uuid']
+  for pattern in patterns: #sys.argv[3:len(sys.argv)]:
+    req_cs = arvados.api().container_requests().list(filters=[["requesting_container_uuid","=",c_uuid], ["name","like", "%"+pattern+"%"]]).execute()
+    for item in req_cs['items']:
+      c_item_uuid = item['container_uuid']
+      c_item_container = arvados.api().containers().list(filters=[["uuid","=",c_item_uuid]]).execute()
+      output_pdh = c_item_container['items'][0]['output']
+      print item['name']
+      for file in Collection(output_pdh):
+        print os.path.join(output_pdh,file)
 
-if re.match('-h.*', sys.argv[1]):
-  print "Usage: python ls_output_collections.py pi_uuid keep_mount job_pattern1 job_pattern2 ..."
-  sys.exit(0)
+def pipeline_instance(uuid, patterns):
 
+  pi_uuid = uuid
+  job_patterns = patterns
+  cluster_uuid = pi_uuid.split('-')[0]
 
-pi_uuid = sys.argv[1]
-keep_mount = sys.argv[2]
-job_patterns = sys.argv[3:]
-cluster_uuid = pi_uuid.split('-')[0]
+  resp = arvados.api().pipeline_instances().list(filters=[["uuid","=", pi_uuid]]).execute()
 
-resp = arvados.api().pipeline_instances().list(filters=[["uuid","=", pi_uuid]]).execute()
+  for job in resp.items()[1][1][0]['components']['cwl-runner']['job']['components']:
+    for pattern in job_patterns:
+      if re.match(pattern, job):
+        job_uuid = resp.items()[1][1][0]['components']['cwl-runner']['job']['components'][job]
+        jobresp = arvados.api().jobs().list(filters=[["uuid", "=", job_uuid]]).execute()
+        output_pdh = jobresp.items()[1][1][0]['output']
+        print job
+        for file in Collection(output_pdh):
+          print os.path.join(output_pdh,file)
 
-for job in resp.items()[1][1][0]['components']['cwl-runner']['job']['components']:
-  for pattern in job_patterns:
-    if re.match(pattern, job):
-      uuid = resp.items()[1][1][0]['components']['cwl-runner']['job']['components'][job]
-      jobresp = arvados.api().jobs().list(filters=[["uuid", "=", uuid]]).execute()
-      output_hash = jobresp.items()[1][1][0]['output']
-      print job
-      print subprocess.check_output(['ls','-Rlah',os.path.join(keep_mount,output_hash)])
-      for file in os.listdir(os.path.join(keep_mount, output_hash)):
-        print os.path.join(output_hash, file)
+def main():
+  if len(sys.argv) <= 2:
+    print "Usage: python ls_output_collections.py [pipeline_instance_uuid or collection_request_uuid] job_pattern1 job_pattern2 ..."
+    sys.exit(0)
+
+  if re.match('-h.*', sys.argv[1]):
+    print "Usage: python ls_output_collections.py [pipeline_instance_uuid or collection_request_uuid] job_pattern1 job_pattern2 ..."
+    sys.exit(0)
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('wf_uuid', metavar='UUID', help='pipeline_instance_uuid or collection_request_uuid')
+  parser.add_argument('patterns', nargs='*', help='job patterns to get output from')
+  args = parser.parse_args()
+
+  if re.match('.*d1hrv.*', args.wf_uuid):
+    pipeline_instance(args.wf_uuid, args.patterns)
+  elif re.match('.*xvhdp.*', args.wf_uuid):
+    container_request(args.wf_uuid, args.patterns)
+  else:
+    print "uuid does not match pipeline instance or container request"
+
+if __name__ == '__main__':
+    main()
+
