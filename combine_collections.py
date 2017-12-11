f27e726667cd3d3956c3a2793b94de1e37ad723e
Bryan Cosca <bcosc@bu.edu>
2017-12-11 20:25:20 +0000

    merge into combine_collections.py

--- combine_collections.py
+++ combine_collections.py
@@ -7,13 +7,16 @@ project_uuid = sys.argv[1]
 
 collection_uuids = []
 for arg in sys.argv[2:len(sys.argv)]:
-  print arg
   collection_uuids.append(arg)
 
 combined_manifest = ""
 for u in collection_uuids:
-    c = api.collections().get(uuid=u).execute()
+    id = arvados.api().collections().list(filters=[["portable_data_hash","=", u]]).execute()
+    uuid = id.items()[1][1][0]['uuid']
+    c = api.collections().get(uuid=uuid).execute()
     combined_manifest += c["manifest_text"]
 
 newcol = arvados.collection.Collection(combined_manifest)
-newcol.save_new(name="foo", owner_uuid=project_uuid)
+newcol.save_new(name="%s" % (str(newcol.keys()[0])), owner_uuid=project_uuid)
+print newcol.portable_data_hash()
+
