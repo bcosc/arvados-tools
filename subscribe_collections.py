0fa2ada456d25063e6c76db7b472420e2e489af4
Bryan Cosca <bcosca@veritasgenetics.com>
2017-12-11 20:27:08 +0000

    subscribe_collections.py works

--- subscribe_collections.py
+++ subscribe_collections.py
@@ -5,9 +5,8 @@ import arvados.events
 
 # 'ev' is a dict containing the log table record describing the change.
 def on_message(ev):
-    print ev
-    #if ev.get("event_type") == "create" and ev.get("object_kind") == "arvados#collection":
-    #print "A new collection was created: %s" % ev["object_uuid"]
+  if ev.get("event_type") == "create" and ev.get("object_kind") == "arvados#collection":
+    print "A new collection was created: %s" % ev["object_uuid"]
 
 api = arvados.api("v1")
 ws = arvados.events.subscribe(api, [["event_type", "=", "create"]], on_message)
