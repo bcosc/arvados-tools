7cc597011c2e7eee2da44074239e65a5148962c9
Bryan Cosca <bcosc@bu.edu>
2017-12-21 16:03:01 +0000

    add project-uuid parameter to grab-directories.py

--- grab-directories.py
+++ grab-directories.py
@@ -5,11 +5,21 @@ import argparse
 from arvados.collection import Collection, Subcollection
 from arvados.arvfile import ArvadosFile
 
-collection_uuid = sys.argv[1]
-c = Collection(collection_uuid)
-dest = Collection()
-for i in c:
-  if isinstance(c[i], Subcollection):
-    dest.copy('./'+i, target_path='./'+i, source_collection=c)
-dest.save_new()
-print(dest.manifest_locator())
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--project-uuid', metavar='UUID', help='Project to output to')
+  parser.add_argument('collection_uuid', metavar='UUID', help='Collection UUID')
+  args = parser.parse_args()
+  collection_uuid = args.collection_uuid
+  project_uuid = args.project_uuid
+  c = Collection(collection_uuid)
+  dest = Collection()
+  for i in c:
+    if isinstance(c[i], Subcollection):
+      dest.copy('./'+i, target_path='./'+i, source_collection=c)
+  dest.save_new(owner_uuid=project_uuid)
+  print(dest.manifest_locator())
+
+if __name__ == '__main__':
+  main()
+
