774d5977a32cff731e055689751de3998dc77c82
Bryan Cosca <bcosc@bu.edu>
2018-01-05 21:52:48 +0000

    added bigger limit to check-trash

--- check-trash-container-request.py
+++ check-trash-container-request.py
@@ -19,7 +19,7 @@ def container_request(uuid):
   cr_uuid = uuid
   cr = arvados.api().container_requests().list(filters=[["uuid", "=", cr_uuid]]).execute()
   c_uuid = cr.items()[1][1][0]['container_uuid']
-  req_cs = arvados.api().container_requests().list(filters=[["requesting_container_uuid","=",c_uuid]]).execute()
+  req_cs = arvados.api().container_requests().list(filters=[["requesting_container_uuid","=",c_uuid]], limit=1000).execute()
   print "CR_UUID, CR NAME, CR_OUTPUT_UUID, TRASHED_AT"
   for item in req_cs['items']:
     cr_output_uuid = item['output_uuid']
